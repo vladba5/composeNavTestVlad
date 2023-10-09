a3ce41f5339ab48ef7a619b25c1285043414c3bd
vlad <vlad.babitsky@tikalk.com>
2023-10-09 11:56:35 +0300

    back stack

--- app/src/main/java/com/example/composenavtestvlad/MainActivity.kt
+++ app/src/main/java/com/example/composenavtestvlad/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.composenavtestvlad
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
@@ -15,6 +16,11 @@ import com.example.composenavtestvlad.two.ScreenTwo
 import com.example.composenavtestvlad.ui.theme.ComposeNavTestVladTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object{
+        const val NAME_ARGS = "NAME_ARGS"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -31,13 +37,19 @@ class MainActivity : ComponentActivity() {
                     navController = navController,
                     startDestination = "screen1"
                 ) {
-                    composable("screen1"){
+                    composable("screen1"){ entry ->
                         ScreenOne(){
+                            val text = entry.savedStateHandle.get<String>(NAME_ARGS)
+                            //add text as args
+                            Log.d("ptt", text.toString())
                             navController.navigate("screen2")
                         }
                     }
                     composable("screen2"){
                         ScreenTwo(){
+                            navController.previousBackStackEntry
+                                ?.savedStateHandle
+                                ?.set(NAME_ARGS, "vlad")
                             navController.popBackStack("screen1", false)
 
                         }
