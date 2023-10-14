828742b59acfbb1500e0aab60568261fb49e5d6e
vlad <vlad.babitsky@tikalk.com>
2023-10-14 17:08:05 +0300

    hilt impl added

---
+++ app/src/main/java/com/example/composenavtestvlad/NavigationApplication.kt
@@ -0,0 +1,8 @@
+package com.example.composenavtestvlad
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NavigationApplication : Application() {
+}
---
+++ app/src/main/java/com/example/composenavtestvlad/navigation/MainNavigation.kt
@@ -0,0 +1,27 @@
+package com.example.composenavtestvlad.navigation
+
+//import androidx.compose.runtime.Composable
+//import androidx.navigation.NavHostController
+//import androidx.navigation.compose.NavHost
+//import androidx.navigation.compose.rememberNavController
+//import com.jetpackcompose.feature.article.nav.articleScreen
+//import com.jetpackcompose.feature.article.nav.navigateToArticle
+//import com.jetpackcompose.feature.home.nav.homeRoute
+//import com.jetpackcompose.feature.home.nav.homeScreen
+//
+//@Composable
+//fun MainNavigation(
+//    navController: NavHostController = rememberNavController(),
+//) {
+//    NavHost(navController = navController, startDestination = homeRoute) {
+//
+//        homeScreen {
+//            navController.navigateToArticle("fakeArticleId")
+//        }
+//
+//        articleScreen {
+//            navController.navigateUp()
+//        }
+//    }
+//
+//}
