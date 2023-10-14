5b34f85a64a82cfd7e776b02b0cea2ed0a266e6f
vlad <vlad.babitsky@tikalk.com>
2023-10-14 17:11:38 +0300

    bug fix

--- feature_one/src/main/java/com/example/composenavtestvlad/feature_one/lobby/LobbyScreen.kt
+++ feature_one/src/main/java/com/example/composenavtestvlad/feature_one/lobby/LobbyScreen.kt
@@ -2,8 +2,8 @@ package com.example.composenavtestvlad.feature_one.lobby
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
